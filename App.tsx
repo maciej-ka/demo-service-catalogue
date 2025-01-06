85d9a401e5f19d4ee14332516722c9fde77082ef
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-01-06 12:12:37 +0100

    feat: progress on styling

--- App.tsx
+++ App.tsx
@@ -4,13 +4,11 @@ import ServiceDetailsPage from "./pages/ServiceDetailsPage/ServiceDetailsPage"
 import HomePage from "./pages/HomePage/HomePage"
 
 const App = () => (
-  <div>
-    <Routes>
-      <Route path="/service/:id/order" Component={ServiceOrderPage} />
-      <Route path="/service/:id" Component={ServiceDetailsPage} />
-      <Route path="/" Component={HomePage} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  </div>
+  <Routes>
+    <Route path="/service/:id/order" Component={ServiceOrderPage} />
+    <Route path="/service/:id" Component={ServiceDetailsPage} />
+    <Route path="/" Component={HomePage} />
+    <Route path="*" element={<Navigate to="/" replace />} />
+  </Routes>
 )
 export default App
--- pages/HomePage/HomePage.tsx
+++ pages/HomePage/HomePage.tsx
@@ -1,10 +1,20 @@
 import styles from './HomePage.module.css'
-// import services from '../../data/data.ts'
+import {services, Service } from '../../data/data'
 
 const HomePage = () => {
-  return (
+  const renderService = (service: Service) => (
     <div>
+      <div className="flex">
+        <h2>{service.name}</h2>
+        price
+      </div>
+    </div>
+  )
+
+  return (
+    <div className={styles.page}>
       <div className={styles.header} />
+      {services.map(renderService)}
     </div>
   )
 }
