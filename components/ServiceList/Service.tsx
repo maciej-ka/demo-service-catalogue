dd704a23be1f04d9f3f90c57055f62ae871c44db
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-01-06 23:12:01 +0100

    refactor: extract Service component

---
+++ components/ServiceList/Service.tsx
@@ -0,0 +1,22 @@
+import { Service as ServiceType } from '../../data/data';
+import Svg from '../ui/Svg/Svg';
+import styles from './Service.module.css';
+
+type Props = {
+  service: ServiceType
+}
+
+const Service = ({service}: Props) => (
+  <div className={styles.service}>
+    <div className={styles.content}>
+      <div className="flex-spread align-center">
+        <h2>{service.name}</h2>
+        <span className={styles.price}>{service.price}</span>
+      </div>
+      <p className={styles.description}>{service.shortDescription}</p>
+    </div>
+    <Svg name={service.background} className={styles.background} />
+  </div>
+)
+
+export default Service
--- pages/HomePage/HomePage.tsx
+++ pages/HomePage/HomePage.tsx
@@ -1,28 +1,16 @@
 import styles from "./HomePage.module.css"
-import { Service } from "../../data/data"
 import { useServices } from "../../data/ServicesProvider"
-import Svg from "../../components/ui/Svg/Svg"
+import Service from "../../components/ServiceList/Service"
 
 const HomePage = () => {
   const services = useServices()
 
-  const renderService = (service: Service) => (
-    <div className={styles.service} key={service.id}>
-      <div className={styles.content}>
-        <div className="flex-spread align-center">
-          <h2>{service.name}</h2>
-          <span className={styles.price}>{service.price}</span>
-        </div>
-        <p className={styles.description}>{service.shortDescription}</p>
-      </div>
-      <Svg name={service.background} className={styles.background} />
-    </div>
-  )
-
   return (
     <div>
       <div className={styles.header} />
-      {services.map(renderService)}
+      <div className={styles.list}>
+        {services.map(service => <Service service={service} key={service.id} />)}
+      </div>
     </div>
   )
 }
