3ed47d3831ec72072431464ec8543d38460049e3
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-01-06 21:01:46 +0100

    feat: position svgs as background on a list

--- pages/HomePage/HomePage.tsx
+++ pages/HomePage/HomePage.tsx
@@ -1,17 +1,21 @@
 import styles from "./HomePage.module.css"
 import { Service } from "../../data/data"
 import { useServices } from "../../components/ServicesProvider"
+import Svg from "../../components/ui/Svg/Svg"
 
 const HomePage = () => {
   const services = useServices()
 
   const renderService = (service: Service) => (
     <div className={styles.service} key={service.id}>
-      <div className="flex-spread align-center">
-        <h2>{service.name}</h2>
-        <span className={styles.price}>{service.price}</span>
+      <div className={styles.content}>
+        <div className="flex-spread align-center">
+          <h2>{service.name}</h2>
+          <span className={styles.price}>{service.price}</span>
+        </div>
+        <p className={styles.description}>{service.shortDescription}</p>
       </div>
-      <p className={styles.description}>{service.shortDescription}</p>
+      <Svg name={service.background} className={styles.background} />
     </div>
   )
 
