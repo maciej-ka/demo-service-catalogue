1315f6f808378372901fe66d6cf53f438e32f020
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-01-07 08:35:27 +0100

    feat: basic file structure for service order

---
+++ components/ServiceOrderWizard/Step1/Step1.tsx
@@ -0,0 +1,9 @@
+import styles from "./Step1.module.css"
+
+const Step1 = () => (
+  <div className={styles.wrapper}>
+    <input type="date" />
+  </div>
+)
+
+export default Step1
--- pages/ServiceOrderPage/ServiceOrderPage.tsx
+++ pages/ServiceOrderPage/ServiceOrderPage.tsx
@@ -1,28 +1,11 @@
-import { Field, Input, Label } from "@headlessui/react"
 import { useParams } from "react-router-dom"
-import { useForm } from "react-hook-form"
-import styles from "./ServiceOrderPage.module.css"
+import ServiceOrderWizard from "../../components/ServiceOrderWizard/ServiceOrderWizard"
 
 const ServiceOrderPage = () => {
   const { id } = useParams()
-  const {
-    register,
-    formState: { errors },
-  } = useForm({ mode: "onBlur" })
+  console.log(id)
 
-  return (
-    <div className={styles.wrapper}>
-      Service Order Page, id: {id}
-      <Field>
-        <Label className={styles.label}>Name</Label>
-        <Input
-          className={styles.input}
-          {...register("ownerName", { required: true })}
-        />
-        {errors.ownerName && <p>Owner name is required.</p>}
-      </Field>
-    </div>
-  )
+  return <ServiceOrderWizard />
 }
 
 export default ServiceOrderPage
